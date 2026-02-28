5e96353465b68382bd780543d1b7c9e3a7228ff1
Rifat <nk.rifat47@gmail.com>
2026-03-01 01:27:04 +0600

    add follow up in prisma and when in creating project

--- src/modules/project/project.validation.js
+++ src/modules/project/project.validation.js
@@ -11,5 +11,10 @@ export const createProjectSchema = (data) => {
     if (!data.conversationStatus) errors.push("conversationStatus is required");
     if (!data.soldById) errors.push("soldById is required");
 
+    // Follow-up Status (Optional but should be booleans)
+    if (data.f01 !== undefined && typeof data.f01 !== "boolean") errors.push("f01 must be a boolean");
+    if (data.f02 !== undefined && typeof data.f02 !== "boolean") errors.push("f02 must be a boolean");
+    if (data.f03 !== undefined && typeof data.f03 !== "boolean") errors.push("f03 must be a boolean");
+
     return errors.length > 0 ? { error: errors.join(", ") } : { error: null };
 };
