c291f5f31b9a3ba846ad33bb29b8f323c7eb977b
Kishor Mahmud <mahmudkishor9@gmail.com>
2026-03-15 02:02:57 +0600

    some changes in database

---
+++ prisma/migrations/20260314200031_init/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [LOW_FOCUS_COUNTRY] on the enum `QueryStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "QueryStatus_new" AS ENUM ('CUSTOM_OFFER_SENT', 'BRIEF_CUSTOM_OFFER_SENT', 'BRIEF_REPLIED', 'QUOTE_SENT', 'FEATURE_LIST_SENT', 'NO_RESPONSE', 'PASS', 'CONVERSATION_RUNNING', 'NONE');
+ALTER TABLE "Project" ALTER COLUMN "queryStatus" TYPE "QueryStatus_new" USING ("queryStatus"::text::"QueryStatus_new");
+ALTER TYPE "QueryStatus" RENAME TO "QueryStatus_old";
+ALTER TYPE "QueryStatus_new" RENAME TO "QueryStatus";
+DROP TYPE "QueryStatus_old";
+COMMIT;
